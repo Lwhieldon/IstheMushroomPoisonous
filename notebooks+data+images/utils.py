c9be49ca539480cff7dc5bf8fad5db4770119876
Lee Whieldon <lewhiel1@umbc.edu>
2020-10-15 18:28:59 -0400

    updated

--- notebooks+data+images/utils.py
+++ notebooks+data+images/utils.py
@@ -14,12 +14,12 @@ def model_visualization(model,X,y,classifier):
     from matplotlib.colors import ListedColormap
     X_set, y_set = X, y
     X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.6, cmap = ListedColormap(('red', 'blue')))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha = 0.6, cmap = ListedColormap(('green', 'blue')))
     plt.xlim(X1.min(), X1.max())
     plt.ylim(X2.min(), X2.max())
     for i, j in enumerate(np.unique(y_set)):
         plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                    color = ListedColormap(('red', 'blue'))(i), label = j)
+                    color = ListedColormap(('turquoise', 'blue'))(i), label = j)
     plt.title("%s Model Set" %(model))
     plt.xlabel('PC 1')
     plt.ylabel('PC 2')
